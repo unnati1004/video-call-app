26b46176ba95d6b201894440fc15baf5b4957315
Vikas Gaikwad <vgaikwad299@gmail.com>
2022-04-22 20:55:12 +0530

    context api done

---
+++ video_app_frontend/src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import {AppBar, Typography} from '@mui/material'
+import Videoplayer from './components/Videoplayer';
+import Options from './components/Options';
+import Notifications from './components/Notifications';
+import { makeStyles } from '@mui/styles';
+
+
+const useStyles=makeStyles((theme) =>({
+ 
+
+ 
+
+}))
+
+
+const App=()=> {
+
+  //  const classes=useStyles()
+
+
+
+  return (
+    <div className="wrapper"> 
+
+    <AppBar className="appBar" >
+      <Typography variant='h2'>
+        Video chat
+      </Typography>
+
+      <Videoplayer />
+
+      <Options>
+        <Notifications/>
+      </Options>
+
+    </AppBar>
+
+
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ video_app_frontend/src/SocketContext.js
@@ -0,0 +1,150 @@
+import React, { createContext, useEffect, useRef, useState } from 'react';
+import {io} from 'socket.io-client'
+import Peer from "simple-peer"
+
+const SocketContext=createContext()
+
+const socket=io("http://localhost:8080")
+
+
+
+const ContextProvider=({children})=>{
+
+    const [stream,setstream]=useState(null)
+
+    const [me, setMe] = useState("")
+
+
+
+    const [call, setcall] = useState({})
+
+    const [callAccepted, setcallAccepted] = useState(false)
+
+    const [callEnded, setcallEnded] = useState(false)
+
+    const [name, setname] = useState("")
+
+
+
+ const myVideo=useRef()
+
+ const userVideo=useRef()
+
+ const connectionRef=useRef()
+
+
+
+
+    useEffect(()=>{
+        navigator.mediaDevices.getUserMedia({video:true,audio:true}).then((currentstream)=>{
+
+            setstream(currentstream)
+
+            myVideo.current.srcObject=currentstream
+
+        })
+
+        socket.on('me',(id)=>{
+            setMe(id)
+        })
+
+        socket.on("calluser",({from,name:callerName,signal})=>{
+
+
+            setcall({isRecievedCall:true,from,name:callerName,signal})
+
+        },[])
+
+    })
+
+    const answercall=()=>{
+
+        setcallAccepted(true)
+
+        const peer=new Peer({initiator:false,trickel:false,stream})
+
+        peer.on("signal",(data)=>{
+
+            socket.emit("answercall",{signal:data,to:call.from})
+
+        })
+
+
+
+        peer.on("stream",(currentstream)=>{
+
+            userVideo.current.srcObject=currentstream
+        })
+
+        peer.signal(call.signal)
+
+
+        connectionRef.current=peer;
+
+
+    }
+
+
+    const calluser=(id)=>{
+          const peer=new Peer({initiator:true,trickel:false,stream})
+
+
+       peer.on("signal",(data)=>{
+
+            socket.emit("calluser",{userTocall:id,signalData:data,from:me,name})
+
+        })
+
+
+
+        peer.on("stream",(currentstream)=>{
+
+            userVideo.current.srcObject=currentstream
+        })
+
+        socket.on("callaccepted",(signal)=>{
+
+            setcallAccepted(true)
+
+
+            peer.signal(signal)
+        })
+
+        connectionRef.current=peer
+
+    }
+    const leavecall=()=>{
+        setcallEnded(true)
+
+         connectionRef.current.destroy();
+
+         window.location.reload();
+
+    }
+
+    return(
+
+        <SocketContext.Provider value={{
+            call,
+            callAccepted,
+            callEnded,
+            myVideo,
+            userVideo,
+            name,
+            setname,
+            stream,
+            me,
+            calluser,
+            leavecall,
+            answercall
+
+        }}>
+            {children}
+
+            
+            </SocketContext.Provider>
+    )
+    
+}
+
+export{ContextProvider,SocketContext}
